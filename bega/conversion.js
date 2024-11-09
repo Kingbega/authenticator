733f3e13eec18510cb56bb9c79ddd14cad3d5428
⏤͟͟͞͞ʙᴀʀᴀᴋᴀ ʙᴇɢᴀ🍀 <barakachachabega@gmail.com>
2024-11-09 09:43:46 +0300

    Update conversion.js

--- bega/conversion.js
+++ bega/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Baraka-Md" ,
+      pack:"Baraka-MD" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
@@ -94,7 +94,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Baraka-Md", // pack stick
+      pack:"Baraka-MD", // pack stick
       author:  nomAuteurMessage, // name of the author of the stick
       type:
         arg.includes("-r") || arg.includes("-c")
