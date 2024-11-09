3a566aad3985514cb3eb90567176bf4c33ef111a
⏤͟͟͞͞ʙᴀʀᴀᴋᴀ ʙᴇɢᴀ🍀 <barakachachabega@gmail.com>
2024-11-09 16:35:00 +0300

    Update heroku.js

--- bega/heroku.js
+++ bega/heroku.js
@@ -48,7 +48,7 @@ zokou(
 			let baseURI = "/apps/" + s.HEROKU_APP_NAME;
 
             let h = await heroku.get(baseURI+'/config-vars')
-let str = '*BARAKA-MD-V2 WABOT VARS*\n\n'
+let str = '*BARAKA-MD WABOT VARS*\n\n'
 for (vr in h) {
 str+= '🚘 *'+vr+'* '+'= '+h[vr]+'\n'
 }
